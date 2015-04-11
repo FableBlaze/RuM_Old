c40b0ba634eb9295da42c77f00e3a076539f5469
Anti Alman <anti666@ut.ee>
2015-04-12 00:59:19 +0300

    Beans are now used in validation. Exception is password reentry validation #9

--- src/ee/ut/cs/rum/domain/Account.java
+++ src/ee/ut/cs/rum/domain/Account.java
@@ -3,28 +3,40 @@ package ee.ut.cs.rum.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
-import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Email;
 
 @Entity
 @Table(name="accounts")
 public class Account {
 	
 	@Id
-    @GeneratedValue(generator="increment")
-    @GenericGenerator(name="increment", strategy = "increment")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-
+	
+	@NotNull
+    @Size(min = 2, max = 24)
 	@Column(name = "name", unique=true)
     private String name;
+	
+	@NotNull
+	@Email
 	@Column(name = "email")
     private String email;
-	@Column(name = "password")
-    private String password;
-	@Column(name = "role")
-    private String role;
+	
+	@NotNull
+    @Size(min = 2, max = 24)
+    @Column(name = "password")
+	private String password;
+	
+	@NotNull
+    @Column(name = "role")
+	private String role;
     
     public Account() {
 	}
--- src/ee/ut/cs/rum/frontpage/CreateAccount.java
+++ src/ee/ut/cs/rum/frontpage/CreateAccount.java
@@ -1,8 +1,11 @@
 package ee.ut.cs.rum.frontpage;
 
+import com.vaadin.data.Item;
 import com.vaadin.data.Validator.InvalidValueException;
-import com.vaadin.data.validator.EmailValidator;
-import com.vaadin.data.validator.StringLengthValidator;
+import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
+import com.vaadin.data.util.BeanItem;
+import com.vaadin.data.validator.BeanValidator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Alignment;
@@ -17,80 +20,124 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.Button.ClickEvent;
 
 import ee.ut.cs.rum.RumUI;
+import ee.ut.cs.rum.domain.Account;
 
 @SuppressWarnings("serial")
 public class CreateAccount extends Panel implements View {
 	public static final String NAME = "createAccount";
 
+	private Item accountItem;
+	private FieldGroup binder;
+
 	private RumUI currentUI; 
 	private Button enterButton;
-	
+
 	private TextField accName;
 	private TextField accEmail;
 	private PasswordField accPassword;
 	private PasswordField rePassword;
-	
+
 	public CreateAccount() {
+		accountItem = new BeanItem<Account>(new Account());
+
 		currentUI = ((RumUI) UI.getCurrent());
-		
+
 		GridLayout layout = new GridLayout(1, 6);
-		
+
 		Label title = new Label("Enter new account details");
 		layout.addComponent(title, 0, 0);
 		layout.setComponentAlignment(title, Alignment.MIDDLE_CENTER);
-		
+
 		accName = new TextField("Account name");
+		accName.setImmediate(true);
+		accName.setValidationVisible(false);
+		accName.setNullRepresentation("");
+		accName.addValidator(new BeanValidator(Account.class, "name"));
 		layout.addComponent(accName, 0, 1);
 		layout.setComponentAlignment(accName, Alignment.MIDDLE_CENTER);
-		
+
 		accEmail = new TextField("Account email");
 		accEmail.setImmediate(true);
+		accEmail.setValidationVisible(false);
+		accEmail.setNullRepresentation("");
+		accEmail.addValidator(new BeanValidator(Account.class, "email"));
 		layout.addComponent(accEmail, 0, 2);
 		layout.setComponentAlignment(accEmail, Alignment.MIDDLE_CENTER);
-		
+
 		accPassword = new PasswordField("Account password");
 		accPassword.setImmediate(true);
+		accPassword.setValidationVisible(false);
+		accPassword.setNullRepresentation("");
+		accPassword.addValidator(new BeanValidator(Account.class, "password"));
 		layout.addComponent(accPassword, 0, 3);
 		layout.setComponentAlignment(accPassword, Alignment.MIDDLE_CENTER);
-		
+
 		rePassword = new PasswordField("Re-enter password");
 		rePassword.setImmediate(true);
+		rePassword.setValidationVisible(false);
+		rePassword.setNullRepresentation("");
+		rePassword.addValidator(new BeanValidator(Account.class, "password"));
 		layout.addComponent(rePassword, 0, 4);
 		layout.setComponentAlignment(rePassword, Alignment.MIDDLE_CENTER);
-		
+
+		accPassword.addValidator(new RePasswordValidator("Passwords must match", accPassword, rePassword));
+		rePassword.addValidator(new RePasswordValidator("Passwords must match", accPassword, rePassword));
+
+
 		Button newAccButton = createNewAccButton();
 		layout.addComponent(newAccButton, 0, 5);
 		layout.setComponentAlignment(newAccButton, Alignment.MIDDLE_CENTER);
-		
+
+
+		binder = new FieldGroup(accountItem);
+		binder.bind(accName, "name");
+		binder.bind(accEmail, "email");
+		binder.bind(accPassword, "password");
+
 		layout.setWidth("100%");
 		layout.setColumnExpandRatio(0, 1);
 		this.setContent(layout);
-		
+
 		createEnterButton("Create new Account");
 	}
-	
+
 	private Button createNewAccButton() {
 		Button button = new Button("Create account");
 		button.setImmediate(true);
 		button.addClickListener(new Button.ClickListener() {
-		    public void buttonClick(ClickEvent event) {
-		    	Notification.show("Account ALMOST created!",  Notification.Type.HUMANIZED_MESSAGE);
-		    	//TODO
-		    }
+			public void buttonClick(ClickEvent event) {
+				accName.setValidationVisible(true);
+				accEmail.setValidationVisible(true);
+				accPassword.setValidationVisible(true);
+				rePassword.setValidationVisible(true);
+
+				try {
+					binder.commit();
+					accName.clear();
+					accEmail.clear();
+					accPassword.clear();
+					rePassword.clear();
+					Notification.show("Account ALMOST created!",  Notification.Type.HUMANIZED_MESSAGE);
+				} catch (CommitException e) {
+					Notification.show("Account details contain errors", "Click to dismiss", Notification.Type.ERROR_MESSAGE);
+					e.printStackTrace();
+					System.out.println(e);
+				}
+			}
 		});
 		return button;
 	}
-	
+
 	private void createEnterButton(String label) {
 		enterButton = new Button(label);
 		enterButton.setImmediate(true);
 		enterButton.addClickListener(new Button.ClickListener() {
-		    public void buttonClick(ClickEvent event) {
-		    	currentUI.getRumNavigator().navigateTo(CreateAccount.NAME);
-		    }
+			public void buttonClick(ClickEvent event) {
+				currentUI.getRumNavigator().navigateTo(CreateAccount.NAME);
+			}
 		});
 	}
-	
+
 	public Button getEnterButton() {
 		return enterButton;
 	}
@@ -99,7 +146,6 @@ public class CreateAccount extends Panel implements View {
 	public void enter(ViewChangeEvent event) {
 		enterButton.setEnabled(false);
 		currentUI.getHeader().setCurrentlyPressed(enterButton);
-		
 	}
-	
+
 }
